d9216b53b3c1a6569b6e2bb83dfc238638e411c6
cella <cella.sum@gmail.com>
2012-09-29 16:47:19 -0400

    update play project

---
+++ target/scala-2.9.1/src_managed/main/controllers/routes.java
@@ -0,0 +1,19 @@
+// @SOURCE:/Users/csum/vdo/conf/routes
+// @HASH:7146bf5ffec26c2f847f18152bb46ae68389afe6
+// @DATE:Sat Sep 29 16:16:03 EDT 2012
+
+package controllers;
+
+public class routes {
+public static final controllers.ReverseApplication Application = new controllers.ReverseApplication();
+public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets();
+public static class javascript {
+public static final controllers.javascript.ReverseApplication Application = new controllers.javascript.ReverseApplication();
+public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets();    
+}   
+public static class ref {
+public static final controllers.ref.ReverseApplication Application = new controllers.ref.ReverseApplication();
+public static final controllers.ref.ReverseAssets Assets = new controllers.ref.ReverseAssets();    
+} 
+}
+                            
